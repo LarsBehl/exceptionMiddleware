02072f45dc0477b181801a352e3d5aa2e5c8512f
Lars Behl <lars.behl@gmx.de>
2021-10-14 13:47:19 +0200

    [FEAT] adde InternalServerErrorException

---
+++ Exceptions/InternalServerErrorException.cs
@@ -0,0 +1,28 @@
+using ExceptionMiddleware.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExceptionMiddleware.Exceptions
+{
+    public class InternalServerErrorException : AppException
+    {
+        private static readonly string TITLE = "Internal Server Error";
+        private static readonly string DEFAULT_MESSAGE = "An unexpected Error occured";
+
+        public override IActionResult ResponseObject => new InternalServerErrorObjectResult(this.GetErrorObject());
+
+        public InternalServerErrorException(string detailMessage, int errorCode) : base(TITLE, detailMessage, errorCode)
+        {
+
+        }
+
+        public InternalServerErrorException(string detailMessage) : this(detailMessage, (int) ErrorCodes.InternalServerError)
+        {
+
+        }
+
+        public InternalServerErrorException() : this(DEFAULT_MESSAGE)
+        {
+
+        }
+    }
+}
