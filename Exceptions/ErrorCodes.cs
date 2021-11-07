adae8638fba16534b20af8be8a230bd372ad3271
Lars Behl <lars.behl@gmx.de>
2021-11-07 18:01:56 +0100

    [FEAT] added ForbiddenException and ForbiddenObjectResult

--- Exceptions/ErrorCodes.cs
+++ Exceptions/ErrorCodes.cs
@@ -4,6 +4,7 @@ namespace ExceptionMiddleware.Exceptions
     {
         BadRequest = 40000,
         Unauthorized = 40100,
+        Forbidden = 40300,
         RessourceNotFound = 40400,
         UserNotFound = 40401,
         InternalServerError = 50000
