dd21f252bd591958ccddc411a0a04675c4fc7e99
Lars Behl <lars.behl@gmx.de>
2022-12-31 13:43:48 +0100

    [FEAT] Added support for request validation

--- Extensions/ServiceCollectionExtensions.cs
+++ Extensions/ServiceCollectionExtensions.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using ExceptionMiddleware.Exceptions;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ExceptionMiddleware.Extensions;
@@ -6,11 +8,44 @@ public static class ServiceCollectionExtensions
 {
     public static IMvcBuilder AddControllersWithExceptions(this IServiceCollection services)
     {
-        return services.AddControllers(options => options.Filters.Add(new ExceptionFilter()));
+        return services.AddControllers()
+                       .AddExceptions()
+                       .AddValidationSupport();
     }
 
     public static IMvcBuilder AddExceptions(this IMvcBuilder builder)
     {
         return builder.AddMvcOptions(options => options.Filters.Add(new ExceptionFilter()));
+    }
+
+    public static IMvcBuilder AddValidationSupport(this IMvcBuilder builder)
+    {
+        return builder.ConfigureApiBehaviorOptions(options =>
+        {
+            options.InvalidModelStateResponseFactory = context =>
+            {
+                StringBuilder bob = new StringBuilder();
+                bool isFirst = true;
+
+                foreach (var modelState in context.ModelState.Values)
+                {
+                    if (modelState.Errors.Count > 0)
+                    {
+                        foreach (var error in modelState.Errors)
+                        {
+                            if (isFirst)
+                            {
+                                bob.Append(error.ErrorMessage);
+                                isFirst = false;
+                            }
+                            else
+                                bob.Append($", {error.ErrorMessage}");
+                        }
+                    }
+                }
+
+                return new BadRequestException(bob.ToString()).ResponseObject;
+            };
+        });
     }
 }
