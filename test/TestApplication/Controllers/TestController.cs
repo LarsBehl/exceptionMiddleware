372d08c843830f2aab11c0bc80bdc81a6172b60c
Lars Behl <lars.behl@gmx.de>
2023-01-22 17:39:12 +0100

    [TEST2] added test to check that error response and code is part of api spec

--- test/TestApplication/Controllers/TestController.cs
+++ test/TestApplication/Controllers/TestController.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using ExceptionMiddleware.Exceptions;
+using ExceptionMiddleware.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ExceptionMiddleware.Test.Application.Controllers;
@@ -7,63 +8,75 @@ namespace ExceptionMiddleware.Test.Application.Controllers;
 [ApiController]
 [Produces("application/json")]
 [Route("test")]
+[ProducesErrorResponseType(typeof(ErrorResponse))]
 public class TestController : ControllerBase
 {
     [HttpGet("badRequest")]
+    [ProducesDefaultResponseType]
     public ActionResult GetBadRequestException()
     {
         throw new BadRequestException();
     }
 
     [HttpGet("forbidden")]
+    [ProducesDefaultResponseType]
     public ActionResult GetForbiddenException()
     {
         throw new ForbiddenException();
     }
 
     [HttpGet("internalServerError")]
+    [ProducesDefaultResponseType]
     public ActionResult GetInternalServerErrorException()
     {
         throw new InternalServerErrorException();
     }
 
     [HttpGet("notFound")]
+    [ProducesDefaultResponseType]
     public ActionResult GetNotFoundException()
     {
         throw new NotFoundException();
     }
 
     [HttpGet("notImplementedError")]
+    [ProducesDefaultResponseType]
     public ActionResult GetNotImplementedErrorException()
     {
         throw new NotImplementedErrorException();
     }
 
     [HttpGet("notImplemented")]
+    [ProducesDefaultResponseType]
     public ActionResult GetNotImplementedException()
     {
         throw new NotImplementedException();
     }
 
     [HttpGet("unauthorized")]
+    [ProducesDefaultResponseType]
     public ActionResult GetUnauthorizedException()
     {
         throw new UnauthorizedException();
     }
 
     [HttpGet("userNotFound")]
+    [ProducesDefaultResponseType]
     public ActionResult GetUserNotFoundException()
     {
         throw new UserNotFoundException();
     }
 
     [HttpGet("arbitrary")]
+    [ProducesDefaultResponseType]
     public ActionResult GetArbitraryException()
     {
         throw new IOException();
     }
 
     [HttpGet("validation")]
+    [ProducesDefaultResponseType]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     public ActionResult GetValidationError([FromQuery, Range(0, 1)] int validationParam)
     {
         return Ok();
--- test/TestApplication/Program.cs
+++ test/TestApplication/Program.cs
@@ -1,4 +1,6 @@
+using System.Reflection;
 using ExceptionMiddleware.Extensions;
+using Microsoft.OpenApi.Models;
 
 namespace ExceptionMiddleware.Test.Application;
 
@@ -11,8 +13,27 @@ public class Program
         // Add services to the container.
 
         builder.Services.AddControllers().AddExceptions().AddValidationSupport();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "Test Application",
+                Description = "A test application"
+            });
+
+            options.AddExceptions();
+        });
 
         var app = builder.Build();
+
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            options.RoutePrefix = string.Empty;
+        });
 
         app.MapControllers();
 
